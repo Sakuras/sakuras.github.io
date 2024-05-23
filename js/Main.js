604d0ffc1216683b2546df1e907d9510d6247da8
Sakuras <547709604@qq.com>
2024-05-23 17:23:54 +0800

    init

---
+++ js/Main.js
@@ -0,0 +1,19 @@
+function dataManage()
+{
+    alert("DataManage.");
+}
+
+function modelTraining()
+{
+    alert("ModelTraining.");
+}
+
+function modelManage()
+{
+    alert("ModelManage.");
+}
+
+function modelDeploy()
+{
+    alert("ModelDeploy.");
+}
